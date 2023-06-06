3de614a69068ec0ffea76ea870ae8198c85a9fcb
Patrick Ludewig <patrickludewig91@gmail.com>
2023-06-06 17:46:32 +0200

    Update client.ts
    you need to specify an URL not only the name

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "@/__generated__/sdk";
-const client = new GraphQLClient("[your_store_name]", {
+const client = new GraphQLClient("https://[your_store_name].swell.store/graphql", {
   headers: {
     Authorization: "[your_public_key]",
   },
